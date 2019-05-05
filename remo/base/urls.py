b2745d2179b44674910e2b4ab189cab33f83096a
Michael Kohler <me@michaelkohler.info>
2019-05-05 17:38:44 +0200

    Remove about/FAQ and link to Wiki for this

--- remo/base/urls.py
+++ remo/base/urls.py
@@ -4,8 +4,6 @@ from django.views.generic import TemplateView
 from remo.base import views as base_views
 
 urlpatterns = [
-    url(r'about/$', TemplateView.as_view(template_name='about.jinja'), name='about'),
-    url(r'faq/$', TemplateView.as_view(template_name='faq.jinja'), name='faq'),
     url(r'^$', base_views.main, name='main'),
     url(r'^capture-csp-violation$', base_views.capture_csp_violation,
         name='capture-csp-violation'),
